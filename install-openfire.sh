3d2f06db436d939bc011cb1e2eada72abf3eed9e
home4lab <87222176+home4lab@users.noreply.github.com>
2021-07-16 18:22:02 +0700

    Update install-openfire.sh

--- install-openfire.sh
+++ install-openfire.sh
@@ -1,1 +1,9 @@
-dpkg -i *.deb
+echo ""
+echo " === INSTALL JAVA RUNTIME === "
+echo ""
+apt install default-jre
+
+echo ""
+echo " === INSTALL OPENFIRE.DEB  === "
+echo ""
+dpkg -i openfire_4.6.4_all.deb
